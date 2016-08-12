aad7e339a0a542b3db47b5415fa3674e265593af
William G Hatch <william@hatch.uno>
2016-08-12 16:11:29 -0600

    WIP - working on lang funcs

--- rash/lang-funcs.rkt
+++ rash/lang-funcs.rkt
@@ -10,6 +10,17 @@
                      ))
 (require "shell-funcs.rkt")
 
+(begin-for-syntax
+  (define-syntax-class not-pipe
+    (pattern (~not (~literal \|))))
+  (define-splicing-syntax-class pipeline-part
+    (pattern (~seq cmd:not-pipe arg:not-pipe ...)
+             #:attr argv #''(cmd arg ...)))
+  (define-splicing-syntax-class pipeline-part/not-first
+    (pattern (~seq (~literal \|) part:pipeline-part)
+             #:attr argv #''(part.cmd part.arg ...)))
+  )
+
 
 (define-syntax (shell-line-parse stx)
   (define-syntax-class not-newline
@@ -23,9 +34,7 @@
     [(shell-line-parse foo:not-newline ...)
      #'(shell-line foo ...)]))
 
-(define-syntax (shell-line stx)
-  (define-syntax-class not-pipe
-    (pattern (~not (~literal \|))))
+(define-syntax (shell-line-old stx)
   (syntax-parse stx
     #:datum-literals (\|)
     [(shell-line) #'(void)]
@@ -34,6 +43,12 @@
     [(shell-line cmd1:not-pipe arg1:not-pipe ... \| cmd2:not-pipe arg2:not-pipe ...)
      #'(rash-pipeline (quote (cmd1 arg1 ...)) (quote (cmd2 arg2 ...)))
      ]))
+
+(define-syntax (shell-line stx)
+  (syntax-parse stx
+    [(shell-line) #'(void)]
+    [(shell-line p1:pipeline-part pn:pipeline-part/not-first ...)
+     #'(rash-pipeline p1.argv pn.argv ...)]))
 
 
 
