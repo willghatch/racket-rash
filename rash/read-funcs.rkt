91cbd350a9958b73ebcbe32a445a68f961819198
William G Hatch <william@hatch.uno>
2016-08-15 13:40:02 -0600

    simplify a little

--- rash/read-funcs.rkt
+++ rash/read-funcs.rkt
@@ -8,27 +8,19 @@
 (require (prefix-in scribble: scribble/reader))
 (require racket/function)
 
-(define (rash-read-syntax* src in)
-  (parameterize ([current-readtable line-readtable])
-    (read-syntax src in)))
-
 (define (parse-at-reader-output argl
                                 #:src [src #f])
-  (let* ([stx? (syntax? argl)]
-         [->datum (if stx? syntax->datum identity)]
-         [finalize (if stx? identity syntax->datum)]
-         [->list (if stx? syntax->list identity)])
-    (finalize
-     (for/fold ([out-list '()])
-               ([str-or-atout (->list argl)])
-       (if (string? (->datum str-or-atout))
-           (append
-            out-list
-            (rash-read-syntax-seq src (open-input-string (->datum str-or-atout))))
-           (append out-list (list str-or-atout)))))))
+  (for/fold ([out-list '()])
+            ([str-or-atout (syntax->list argl)])
+    (if (string? (syntax->datum str-or-atout))
+        (append
+         out-list
+         (rash-read-syntax-seq src (open-input-string (syntax->datum str-or-atout))))
+        (append out-list (list str-or-atout)))))
 
 (define (rash-read-syntax-seq src in)
-  (let ([result (rash-read-syntax* src in)])
+  (let ([result (parameterize ([current-readtable line-readtable])
+                  (read-syntax src in))])
     (if (equal? eof result)
         '()
         (cons result (rash-read-syntax-seq src in)))))
