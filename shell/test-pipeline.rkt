6468b5a26334540b6a30482f3efd834c9c731551
William G Hatch <william@hatch.uno>
2016-08-18 17:58:05 -0600

    add some lame tests

--- shell/test-pipeline.rkt
+++ shell/test-pipeline.rkt
@@ -3,32 +3,57 @@
 (require "pipeline.rkt")
 (require racket/string)
 (require racket/function)
+(require racket/format)
+(require racket/runtime-path)
 
-#|
-TODO
-write real tests that are reproducible independent of the machine.
-Put ones that don't access the file system (don't run subprocesses)
-into a test submodule.
-|#
+(define-runtime-path pipeline.rkt "pipeline.rkt")
 
 
 (define (grep-func str regex)
-  (string-append
-   (string-join (filter identity
-                        (for/list ([line (string-split str "\n")])
-                          (and (regexp-match regex line) line)))
-                "\n")
-   "\n"))
+  (let ([r (cond [(regexp? regex) regex]
+                 [(~a regex)])])
+    (string-append
+     (string-join (filter identity
+                          (for/list ([line (string-split str "\n")])
+                            (and (regexp-match r line) line)))
+                  "\n")
+     "\n")))
 (define my-grep (shellify grep-func))
 
+(module+ test
+  ;; Only tests that only rely on functions should go here -- calling
+  ;; external programs will fail in the test environment.
+  (require rackunit)
 
-(run-pipeline '(ls -l /dev) `(,my-grep "uucp"))
-(run-pipeline '(ls -l /dev) `(grep "uucp"))
-(define d (alias-func (λ args (list* 'ls '--color=always args))))
-(current-shell-functions
- (hash "ls" (alias-func (λ args (list* (string->path "ls") '--color=auto args)))))
-;(run-pipeline `(,d -l /dev))
-(run-pipeline `(ls -l /dev))
+  (check-equal? (run-pipeline/out '(echo hello))
+                "hello\n" )
+  (check-equal? (run-pipeline/out '(echo hello "\n" how are you? "\n" I am fine)
+                                  `(,my-grep hello))
+                "hello \n")
+  (shell-alias 'aoeu '(echo aoeu))
+  (check-equal? (run-pipeline/out '(aoeu hello))
+                "aoeu hello\n" )
 
-(run-pipeline/out `(ls) (pipeline-member-spec '(grep shell) 'stdout))
+  )
 
+(module+ main
+  ;; Here can go tests that rely on external programs,
+  ;; but these tests must be run manually.
+
+  (require rackunit)
+
+
+  ;; TODO - how to I wrap this so I get a nice summary as with raco test and the test module?
+  (printf "If this exits without saying things passed, then things failed.~n")
+
+  (check-equal? (string->number
+                 (string-trim
+                  (run-pipeline/out `(cat ,pipeline.rkt)
+                                    '(grep define)
+                                    `(,my-grep current-shell-functions)
+                                    '(wc -l))))
+                1)
+
+  (printf "If it didn't say anything about falures, then the tests passed.~n")
+
+  )
