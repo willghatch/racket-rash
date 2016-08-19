ef0181aea373545c3d7ca533107b51eb9c2dc6af
William G Hatch <william@hatch.uno>
2016-08-18 18:08:46 -0600

    add package dep for shell-pipeline

--- info.rkt
+++ info.rkt
@@ -4,6 +4,7 @@
 (define deps '("base"
                "at-exp-lib"
                "basedir"
+               "shell-pipeline"
                "scribble-lib"
                "scribble-doc"
                "racket-doc"
