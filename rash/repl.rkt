402abafa1c90c2187d10904fcc5cc22ff613ea43
William G Hatch <william@hatch.uno>
2016-08-20 19:25:17 -0600

    improve the prompt

--- rash/repl.rkt
+++ rash/repl.rkt
@@ -1,34 +1,50 @@
 #lang racket/base
 
-(require rash)
-(require (prefix-in scribble: scribble/reader))
-(require rash/private/read-funcs)
-(require basedir)
 (require
+ rash
+ (prefix-in scribble: scribble/reader)
+ rash/private/read-funcs
+
+ basedir
+ racket/exn
+ racket/date
+
  (for-syntax syntax/parse
              racket/base))
 
 (define-namespace-anchor ns-a)
 (define ns (namespace-anchor->namespace ns-a))
 
-(define (rash-repl prev-lines-str last-ret-val)
-  (printf "~a > " last-ret-val)
+
+(define (basic-prompt last-ret)
+  (let* ([cdate (current-date)]
+         [chour (date-hour cdate)]
+         [cmin (date-minute cdate)])
+    (printf "~a:~a ~a~n｢~a｣ ~a "
+            chour cmin
+            (path->string (current-directory))
+            last-ret
+            (if (equal? (system-type 'os) 'windows) ">" "➤"))))
+(define current-prompt-function (make-parameter basic-prompt))
+
+(define (rash-repl last-ret-val)
+  ((current-prompt-function) last-ret-val)
   (let* ([next-line (read-line)]
          [exit? (if (equal? next-line eof) (exit) #f)]
-         [input (string-append prev-lines-str next-line)]
-         [read-input (with-handlers ([(λ (ex) #t) (λ (ex) 'retry-line)])
+         [read-input (with-handlers ([(λ (ex) #t)
+                                      (λ (ex) 'cantparse)])
                        ;; TODO - I really only want to keep adding lines if
                        ;; the exeption is that it needs a ) to close a (...
-                       (scribble:read-inside (open-input-string input)))]
+                       (scribble:read-inside (open-input-string next-line)))]
          )
-    (if (equal? read-input 'retry-line)
-        (rash-repl input "line-not-finished")
+    (if (equal? read-input 'cantparse)
+        (rash-repl "Couldn't parse input.")
         (let ([ret-val
                (with-handlers ([(λ (e) #t) (λ (e) e)])
                  (eval `(rash-line-parse
                          ,@(rash-parse-at-reader-output read-input))
                        ns))])
-          (rash-repl "" ret-val)))))
+          (rash-repl ret-val)))))
 
 (define (eval-rashrc rcfile)
   (eval `(rash-line-parse ,@(rash-parse-at-reader-output
@@ -37,12 +53,13 @@
 
 (for ([rcfile (list-config-files #:program "rash" "rashrc")])
   (with-handlers ([(λ _ #t) (λ (ex)
-                              (eprintf "error in rc file ~a: ~a" rcfile ex))])
+                              (eprintf "error in rc file ~a: ~a"
+                                       rcfile (exn->string ex)))])
     (eval-rashrc rcfile)))
 
 (with-handlers ([(λ _ #t) (λ (ex)
-                            (eprintf "Exception: ~a~n" ex)
-                            (rash-repl "" #f))])
-  (rash-repl "" #f))
+                            (eprintf "Exception: ~a~n" (exn->string ex))
+                            (rash-repl #f))])
+  (rash-repl #f))
 
 (printf "and now exiting for some reason~n")
