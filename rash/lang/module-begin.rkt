02b975408003a0dccfe9e5d94e793f830706cac7
William G Hatch <william@hatch.uno>
2016-10-04 13:29:27 -0600

    don't auto-exit with last return -- allow interactions afterward

--- rash/lang/module-begin.rkt
+++ rash/lang/module-begin.rkt
@@ -11,26 +11,8 @@
          (for-syntax racket/base
                      syntax/parse))
 
-(define last-ret (make-parameter 0))
-
-(define-syntax (update-last-ret stx)
-  (syntax-parse stx
-    [(ulr expr)
-     #'(last-ret expr)]))
-
-(define-syntax wrapped-module-begin
-  (make-wrapping-module-begin #'update-last-ret))
 
 (define-syntax (module-begin stx)
   (syntax-parse stx
     [(m-b arg ...)
-     #'(wrapped-module-begin
-        (rash-line-parse arg ...)
-        (cond
-          [(and (exact-nonnegative-integer? (last-ret))
-                (< (last-ret) 128))
-           (exit (last-ret))]
-          [(void? (last-ret)) (exit 0)]
-          [else (begin
-                  (eprintf "improper exit value: ~s~n" (last-ret))
-                  (exit 126))]))]))
+     #'(#%plain-module-begin (rash-line-parse arg ...))]))
