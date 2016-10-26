3a6f6584e16b4bf2b0de8fceee6222d0f70087b9
William G Hatch <william@hatch.uno>
2016-10-26 15:21:25 -0600

    make fancier prompt, add interactive result referencing

--- rash/private/repl-namespace.rkt
+++ rash/private/repl-namespace.rkt
@@ -2,6 +2,38 @@
 (require "../main.rkt")
 (require (submod "lang-funcs.rkt" for-module-begin))
 
+(define interactive-return-values (make-hash))
+(define (result-n n)
+  (hash-ref interactive-return-values n))
+(define (return-n n)
+  (let ([pline (result-n n)])
+    (if (pipeline? pline)
+        (pipeline-status pline)
+        pline)))
+#|
+TODO --
+Probably the hash table should only be modifiable via auto-update
+when commands are run, and only accessing functions should be in
+the repl namespace.
+
+What functions for refering to previous returns would be useful?
+Probably there should be something like (return-n-reverse 0)
+that gives the previously returned.
+
+Add something that refers to results that are bg pipelines.  There should
+be a function to list currently running jobs by their numbers and the command
+they are running.
+
+If I add `pipeline-stop` and `pipeline-resume` functions and have interactive
+pipelines always run in the background, I can add suspend/background/foreground
+to the interactive shell.
+
+bash and friends give jobs numbers as well as their return numbers.
+Is that something useful?  I think it probably isn't worth having two
+numbering schemes...
+
+|#
+
 (define-namespace-anchor ns-a)
 (define repl-namespace (namespace-anchor->namespace ns-a))
 (provide repl-namespace)
--- rash/repl.rkt
+++ rash/repl.rkt
@@ -19,45 +19,68 @@
   (cond [(exn? last-ret)
          (begin (eprintf "~a~n" last-ret)
                 ;; let any filtering output finish
-                (sleep 0)
-                "exn!")]
+                (sleep 0.01)
+                (red "exn!"))]
         [(pipeline? last-ret)
-         (format-ret (pipeline-status last-ret))]
-        [(void? last-ret) ""]
+         ({if (pipeline-success? last-ret)
+              green
+              red}
+          ;(format-ret (pipeline-status last-ret))
+          (format "~s" last-ret)
+          )]
         [else (format "~s" last-ret)]))
 
-(define (basic-prompt last-ret)
+(define (mstyle n)
+  (λ (s) (format "\033[~am~a" n s)))
+(define (mstyle2 n1 n2)
+  (λ (s) (format "\033[~a;~am~a" n1 n2 s)))
+(define default-style (mstyle 0))
+(define cyan (mstyle 36))
+(define red (mstyle 31))
+(define green (mstyle 32))
+(define bblue (mstyle2 1 34))
+
+(define (basic-prompt last-ret last-ret-n)
   (let* ([cdate (current-date)]
          [chour (date-hour cdate)]
          [cmin (date-minute cdate)]
          [padded-min (if (< cmin 10)
                          (string-append "0" (number->string cmin))
                          cmin)]
-         [ret-show (format-ret last-ret)])
-    (printf "~a:~a ~a~n｢~a｣ ~a "
-            chour padded-min
-            (path->string (current-directory))
-            ret-show
-            (if (equal? (system-type 'os) 'windows) ">" "➤"))))
+         [ret-show (green (format-ret last-ret))])
+    (printf "~a:~a ~a ~a~a ~a~a~n~a "
+            (cyan chour) padded-min
+            (bblue (path->string (current-directory)))
+            (default-style "｢R") last-ret-n ret-show (default-style "｣")
+            (default-style (if (equal? (system-type 'os) 'windows) ">" "➤")))))
 (define current-prompt-function (make-parameter basic-prompt))
 
-(define (rash-repl last-ret-val)
-  ((current-prompt-function) last-ret-val)
+(define (rash-repl last-ret-val n)
+  ((current-prompt-function) last-ret-val n)
   (flush-output (current-output-port))
   (flush-output (current-error-port))
   (let* ([next-input (with-handlers ([exn? (λ (e) (eprintf "~a~n" e)
                                               #`(%%rash-racket-line (void)))])
                        (rash-read (current-input-port)))]
          [exit? (if (equal? next-input eof) (exit) #f)])
-    (let ([ret-val
-           (with-handlers ([(λ (e) #t) (λ (e) e)])
-             (eval `(rash-line-parse
-                     ,next-input)
-                   repl-namespace))])
+    (let* ([ret-val-list
+            (call-with-values (λ () (with-handlers ([(λ (e) #t) (λ (e) e)])
+                                      (eval `(rash-line-parse
+                                              ,next-input)
+                                            repl-namespace)))
+                              list)]
+           [ret-val (if (equal? (length ret-val-list)
+                                1)
+                        (car ret-val-list)
+                        ret-val-list)]
+           [new-n (add1 n)])
+      (hash-set! (eval 'interactive-return-values repl-namespace)
+                 new-n
+                 ret-val)
       ;; Sleep just long enough to give any filter ports (eg a highlighted stderr)
       ;; to be able to output before the next prompt.
       (sleep 0.01)
-      (rash-repl ret-val))))
+      (rash-repl ret-val new-n))))
 
 (define (eval-rashrc.rkt rcfile)
   (eval `(require (file ,(path->string rcfile))) repl-namespace))
@@ -77,9 +100,6 @@
                                        rcfile (exn->string ex)))])
     (eval-rashrc rcfile)))
 
-(with-handlers ([(λ _ #t) (λ (ex)
-                            (eprintf "Exception: ~a~n" (exn->string ex))
-                            (rash-repl #f))])
-  (rash-repl #f))
+(rash-repl (void) 0)
 
 (printf "and now exiting for some reason~n")
