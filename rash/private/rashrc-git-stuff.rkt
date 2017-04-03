b959dbda41c76ba7ba87d98e88bef111a0fcede8
William G Hatch <william@hatch.uno>
2017-04-03 15:56:38 -0600

    add some git prompt stuff

---
+++ rash/private/rashrc-git-stuff.rkt
@@ -0,0 +1,79 @@
+#lang racket/base
+
+(provide
+
+ get-git-info
+
+ get-git-root
+ get-current-git-branch
+ git-behind/ahead-numbers
+ git-dirty?
+ git-submodule-dirty?
+ git-has-untracked?
+ )
+
+(require shell/pipeline
+         racket/string
+         racket/port
+         racket/list
+         )
+
+(define (get-git-root dir)
+  (cond [(directory-exists? (build-path dir ".git")) dir]
+        [else (let-values ([(parent end-path must-be-dir) (split-path dir)])
+                (if (path? parent)
+                    (get-git-root parent)
+                    #f))]))
+
+(define (get-current-git-branch)
+  (run-pipeline/out '(git branch)
+                    (list (λ ()
+                            (let ([bs (filter (λ (l) (string-prefix? l "*"))
+                                              (port->lines))])
+                              (if (null? bs)
+                                  (void) 
+                                  (display (substring (car bs) 2))))))))
+
+(define (git-behind/ahead-numbers)
+  (let* ([lrs
+          (run-pipeline/out '(git rev-list --left-right "@{u}...HEAD")
+                            (list (λ ()
+                                    (for ([l (port->lines)])
+                                      (display (string-ref l 0))))))]
+         [lr-list (string->list lrs)])
+    ;; < is behind > is ahead
+    (values (count (λ (c) (equal? #\< c)) lr-list)
+            (count (λ (c) (equal? #\> c)) lr-list))))
+
+(define (git-dirty?)
+  (let ([pline (run-pipeline '(git diff --quiet --ignore-submodules HEAD)
+                             #:in 'null
+                             #:out 'null
+                             #:default-err 'null
+                             )])
+    (not (pipeline-success? pline))))
+
+(define (git-submodule-dirty?)
+  (pipeline-status (run-pipeline '(git submodule summary -n 1)
+                                 (list (λ () (not (equal? "" (port->string))))))))
+
+(define (git-has-untracked?)
+  (let ([out (run-pipeline/out '(git ls-files --other --directory --exclude-standard))])
+    (not (equal? out ""))))
+
+(define (get-git-info)
+  (define root (get-git-root (current-directory)))
+  (if (not root)
+      #f
+      (let ()
+        (define-values (behind ahead) (git-behind/ahead-numbers))
+        (define dirty? (git-dirty?))
+        (define sub-dirty? (git-submodule-dirty?))
+        (hash 'root root
+              'branch (get-current-git-branch)
+              'behind behind
+              'ahead ahead
+              'dirty? (git-dirty?)
+              'submodule-dirty? (git-submodule-dirty?)
+              'untracked? (git-has-untracked?)
+              ))))
--- rash/private/rashrc-lib.rkt
+++ rash/private/rashrc-lib.rkt
@@ -4,6 +4,7 @@
 
 (require racket/date)
 (require shell/pipeline)
+(require "rashrc-git-stuff.rkt")
 
 
 (define (format-ret last-ret)
@@ -33,6 +34,37 @@
 (define green (mstyle 32))
 (define bblue (mstyle2 1 34))
 
+(define windows? (equal? (system-type 'os) 'windows))
+
+(define (git-info-with-style)
+  #|
+  TODO - getting git info may be slow depending on file system.
+  Eg. network mounts or busy file systems may make git info queries
+  unusably slow, or huge git histories may slow this down.  I should
+  add some sort of timeout, or only give information that comes out
+  quickly.
+  |#
+  (define info (get-git-info))
+  (if info
+      (string-append
+       (default-style "[")
+       (hash-ref info 'branch) " "
+       (if (< 0 (hash-ref info 'ahead))
+           (format "~a~a" (default-style "▲") (cyan (hash-ref info 'ahead)))
+           "")
+       (if (< 0 (hash-ref info 'behind))
+           (format "~a~a" (default-style "▼") (cyan (hash-ref info 'behind)))
+           "")
+       (if (hash-ref info 'dirty?) (red " D") "")
+       (if (hash-ref info 'submodule-dirty?) (red " S") "")
+       (if (hash-ref info 'untracked?) (red " U") "")
+       (default-style "] "))
+      ""))
+
+;; TODO - add path coloring like in megaprompt, maybe with some more info and color options
+;; TODO - add path shortening to a maximum length, or more generally finding the
+;; max length a prompt string should be and adjusting all parts to it...
+
 (define (basic-prompt #:last-return-value [last-ret #f]
                       #:last-return-index [last-ret-n #f])
   (let* ([cdate (current-date)]
@@ -42,10 +74,25 @@
                          (string-append "0" (number->string cmin))
                          cmin)]
          [ret-show (green (format-ret last-ret))])
-    (printf "~a:~a ~a ~a~a ~a~a~n~a "
+    (printf "~a:~a ~a~a~a~n~a "
             (cyan chour) padded-min
+            (git-info-with-style)
             (bblue (path->string (current-directory)))
-            (default-style "｢R") last-ret-n ret-show (default-style "｣")
-            (default-style (if (equal? (system-type 'os) 'windows) ">" "➤")))))
+            (if (and last-ret (< 0 last-ret-n))
+                (format " ~a~a ~a~a"
+                        (default-style "｢R")
+                        last-ret-n
+                        ret-show
+                        (default-style "｣"))
+                "")
+            (default-style "➤"))))
 
-(define current-prompt-function (make-parameter basic-prompt))
+(define (lame-prompt #:last-return-value [last-ret #f]
+                     #:last-return-index [last-ret-n #f])
+  (when (and last-ret (< 0 last-ret-n))
+    (printf "[~a ~a~a~n" last-ret-n (format-ret last-ret) (default-style "]")))
+  (printf ">"))
+
+(define current-prompt-function (make-parameter (if windows?
+                                                    lame-prompt
+                                                    basic-prompt)))
