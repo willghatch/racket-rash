e73c8d424bae4fe321704b5b7e64f8ee5fc04a84
William G Hatch <william@hatch.uno>
2017-10-05 09:52:08 -0600

    fix provides for pipeline macro -- add mixed pipeline except for run-pipeline

--- shell/pipeline-macro.rkt
+++ shell/pipeline-macro.rkt
@@ -4,10 +4,13 @@
  (all-from-out (submod "private/pipeline-macro-parse.rkt" for-public))
  (all-from-out "private/pipeline-operator-default.rkt")
  (all-from-out "private/pipeline-operators.rkt")
+ (all-from-out "mixed-pipeline.rkt")
  ;; TODO - some things from pipeline-operator-detect.rkt should be provided
  )
 (require
  (submod "private/pipeline-macro-parse.rkt" for-public)
  "private/pipeline-operator-default.rkt"
  "private/pipeline-operators.rkt"
+ (except-in "mixed-pipeline.rkt"
+            run-pipeline)
  )
