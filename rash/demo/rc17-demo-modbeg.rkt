b639542b0948d80b158ffe2457daa8836d16bc19
William G Hatch <william@hatch.uno>
2017-10-06 21:56:44 -0600

    add definitions to rc17 demo language

--- rash/demo/rc17-demo-modbeg.rkt
+++ rash/demo/rc17-demo-modbeg.rkt
@@ -6,6 +6,10 @@
  (except-out (all-from-out racket/base) #%module-begin)
  (rename-out [basic-rash-module-begin #%module-begin])
  (all-from-out rash)
+
+ app
+ def
+ id
  )
 
 (require
@@ -14,7 +18,9 @@
  rash
  (submod rash/private/lang-funcs for-module-begin)
  (for-syntax
-  racket/base))
+  racket/base
+  syntax/parse
+  ))
 
 (define-syntax basic-rash-module-begin
   (make-rash-module-begin-transformer #:in (current-input-port)
@@ -23,3 +29,13 @@
                                       #:default-starter #'=quoting-basic-unix-pipe=
                                       ;#:default-line-macro #'pipeline-line-macro
                                       ))
+
+
+;;; additional demo definitions
+
+(define-line-macro app (syntax-parser [(_ arg ...) #'(arg ...)]))
+(define-line-macro def (syntax-parser [(_ def-form arg ...)
+                                       #'(define def-form
+                                           (do-line-macro arg ...))]))
+
+(define-line-macro id (syntax-parser [(_ e) #'e]))
