00eaf079959f287ffa044569e3fa4a94b8c3ad63
William G Hatch <william@hatch.uno>
2018-06-20 11:58:33 -0600

    improve error reporting for default pipeline operator

--- shell-pipeline/private/pipeline-operator-default.rkt
+++ shell-pipeline/private/pipeline-operator-default.rkt
@@ -9,13 +9,16 @@
  racket/stxparam
  (for-syntax
   racket/base
+  syntax/parse
   "pipeline-operator-detect.rkt"))
 
 (define-for-syntax (default-pipeline-error stx)
-  (raise-syntax-error
-   'erroring-default-pipeline-starter
-   "No explicit pipeline starter given in a context with no default set."
-   stx))
+  (syntax-parse stx
+    [(_ e1 e2 ...)
+     (raise-syntax-error
+      'erroring-default-pipeline-starter
+      "No explicit pipeline starter given in a context with no default set."
+      #'e1)]))
 
 (define-syntax default-pipeline-starter-macro
   (rash-pipeline-operator default-pipeline-error
