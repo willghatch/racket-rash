14064f2ba5cdf26884298182058cdb80dee1f5b0
William G Hatch <william@hatch.uno>
2019-04-19 19:18:48 +0000

    add dependency on syntax-generic2

--- shell-pipeline/info.rkt
+++ shell-pipeline/info.rkt
@@ -5,6 +5,7 @@
                "scribble-lib"
                "racket-doc"
                "rackunit-lib"
+               "syntax-generic2"
                ))
 (define scribblings '(["scribblings/shell-pipeline.scrbl" (multi-page) (library)]))
 (define version "0.2")
