29ee6d58c836f896b5bb9642d8ce8897da2ae707
William G Hatch <william@hatch.uno>
2019-04-23 23:05:14 +0000

    WIP - rearrange some things in preparation for implementing =bind=

--- shell-pipeline/private/pipeline-operator-generics.rkt
+++ shell-pipeline/private/pipeline-operator-generics.rkt
@@ -43,6 +43,18 @@
   (define-syntax-generic pipeline-starter-macro)
   (define-syntax-generic pipeline-joint-macro)
 
+  #|
+  ?? Does <syntax-generic>? return #t for an identifier bound to it or to an application form using it?
+
+  (my-generic? #'id)
+  (my-generic? #'(id arg ...))
+
+  Both, apparently!  The predicate is supposed to match on the same
+  things that the Racket macro expander matches on when detecting macro
+  uses.  So it matches the form for identifier macros as well as for
+  head-of-list macros.
+  |#
+
   ;; For splitting macro to delimit pipeline segments
   (define-syntax-class pipeline-starter
     (pattern op:id #:when (or (core-pipeline-starter? #'(op))
@@ -54,36 +66,42 @@
     (pattern (~and (~not x:pipeline-joint)
                    (~not x:pipeline-starter))))
 
-  #|
-  ?? Does <syntax-generic>? return #t for an identifier bound to it or to an application form using it?
 
-  (my-generic? #'id)
-  (my-generic? #'(id arg ...))
 
-  Both, apparently!
-  |#
+
+  (define (pipeline-starter->core stx)
+    (cond
+      [(core-pipeline-starter? stx) stx]
+      [(pipeline-starter-macro? stx)
+       (pipeline-starter->core
+        (apply-as-transformer pipeline-starter-macro 'expression #f stx))]
+      [else (error 'pipeline-starter->core "not a pipeline starter ~a\n" stx)]))
+  (define (pipeline-joint->core stx)
+    (cond
+      [(core-pipeline-joint? stx) stx]
+      [(pipeline-joint-macro? stx)
+       (pipeline-joint->core
+        (apply-as-transformer pipeline-joint-macro 'expression #f stx))]
+      [else (error 'pipeline-joint->core "not a pipeline joint ~a\n" stx)]))
 
   (define (dispatch-pipeline-starter stx)
-    (cond
-      ;; Do core form transformation
-      [(core-pipeline-starter? stx)
-       (apply-as-transformer core-pipeline-starter 'expression #f stx)]
-      ;; Reduce macros to core forms
-      [(pipeline-starter-macro? stx)
-       (dispatch-pipeline-starter
-        (apply-as-transformer pipeline-starter-macro 'expression #f stx))]
-      [else (error 'dispatch-pipeline-starter "not a pipeline starter ~a\n" stx)]))
+    (define core-stx (pipeline-starter->core stx))
+    (apply-as-transformer core-pipeline-starter 'expression #f core-stx))
   (define (dispatch-pipeline-joint stx)
-    (cond
-      ;; Do core form transformation
-      [(core-pipeline-joint? stx)
-       (apply-as-transformer core-pipeline-joint 'expression #f stx)]
-      ;; Reduce macros to core forms
-      [(pipeline-joint-macro? stx)
-       (dispatch-pipeline-joint
-        (apply-as-transformer pipeline-joint-macro 'expression #f stx))]
-      [else (error 'dispatch-pipeline-joint "not a pipeline joint ~a\n" stx)]))
+    (define core-stx (pipeline-joint->core stx))
+    (apply-as-transformer core-pipeline-joint 'expression #f core-stx))
   )
+
+;; basic definition form, wrapped by the better one in "pipeline-operators.rkt"
+(define-syntax define-pipeline-operator/no-kw
+  (syntax-parser [(_ name as-starter as-joint outside-of-rash)
+                  #'(define-syntax name
+                      (generics
+                       [pipeline-starter-macro as-starter]
+                       [pipeline-joint-macro as-joint]
+                       ;; TODO - how to do this with syntax-generics?  It was in the paper, but I don't see it in the library.
+                       ;[racket-macro outside-of-rash]
+                       ))]))
 
 (define-syntax (transform-starter-segment stx)
   (syntax-parse stx [(_ arg ...) (dispatch-pipeline-starter #'(arg ...))]))
@@ -135,13 +153,16 @@
    [core-pipeline-starter basic-unix-pipe-transformer]
    [core-pipeline-joint basic-unix-pipe-transformer]))
 
+#;(define-syntax =bind=
+  (generics
+   [core-pipeline-starter
+    (syntax-parser [(~and stx (_ arg1 arg ...))
+                    (raise-syntax-error '=bind=
+                                        "Can't be used as a pipeline starter"
+                                        #'stx
+                                        #'arg1)])]
+   [core-pipeline-joint
+    (λ (stx)
+      ;; TODO - this should call the `bind!` function.  To do that I need a defenition context made with make-def-ctx (which should have a longer name).  I also need a fresh scope probably.  So maybe I need to thread these things through the splitter function?  And this needs different handling for the first-class-pipeline-spec generator case vs the run-pipeline case (the first-class case should allow bindings to be seen later in the pipeline, but nowhere else).  And I need special handling when I'm in an expression context vs a definition context -- in a definition context I should collect all the bindings and re-bind them outside, maybe with a define-values form that is just assigned to the inner version of the variables.
+      aoeu)]))
 
-(define-syntax define-pipeline-operator/no-kw
-  (syntax-parser [(_ name as-starter as-joint outside-of-rash)
-                  #'(define-syntax name
-                      (generics
-                       [pipeline-starter-macro as-starter]
-                       [pipeline-joint-macro as-joint]
-                       ;; TODO - how to do this with syntax-generics?  It was in the paper, but I don't see it in the library.
-                       ;[racket-macro outside-of-rash]
-                       ))]))
